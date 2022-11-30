8f4f365b561dff22b9824a20b8519639d77889a7
MOHAMED GAMIL ELDIMARDASH <gigiyoyo2001@yahoo.com>
2022-11-30 02:17:08 -0600

    Add linters

--- js/index.js
+++ js/index.js
@@ -1,48 +1,48 @@
-"use strict";
-
 const options = {
   method: 'GET',
   headers: {
     'X-RapidAPI-Key': '9ded418773mshb7add42b953ffcap17120ejsn63e01fb3cfef',
-    'X-RapidAPI-Host': 'free-football-soccer-videos.p.rapidapi.com'
-  }
+    'X-RapidAPI-Host': 'free-football-soccer-videos.p.rapidapi.com',
+  },
 };
 
-function fetchData() {
-  fetch('https://free-football-soccer-videos.p.rapidapi.com/', options)
-    .then(response => response.json())
-    .then(response => showdata(response))
-}
+const HEADING = document.querySelector('h1');
+const HEADING2 = document.querySelector('h2');
+const HEADING3 = document.querySelector('h3');
 
-let HEADING = document.querySelector("h1");
-let HEADING2 = document.querySelector("h2");
-let HEADING3 = document.querySelector("h3");
+const video = document.getElementById('video');
+const image = document.getElementById('image');
 
-let video = document.getElementById("video");
-let image = document.getElementById("image");
-
-let btn = document.getElementById("submit");
+const btn = document.getElementById('submit');
 
 function showdata(data) {
-  let randNum = Math.round(Math.random() * 87)
-  console.log(data[randNum]);
-  let {
+  const randNum = Math.round(Math.random() * 89);
+  const {
     title,
     competition,
     date,
     embed,
-    thumbnail
+    thumbnail,
   } = data[randNum];
-  HEADING.innerHTML = competition["name"];
-  HEADING2.innerHTML = "Teams: " + title;
-  HEADING3.innerHTML = "Date: " + date;
+  HEADING.innerHTML = competition.name;
+  HEADING2.innerHTML = `Teams: ${title}`;
+
+  const tIndex = date.indexOf('T');
+  const date1 = date.slice(0, tIndex);
+  HEADING3.innerHTML = `Date: ${date1}`;
 
   image.src = thumbnail;
-  image.setAttribute("class", "img");
-  image.setAttribute("alt", "match-image");
+  image.setAttribute('class', 'img');
+  image.setAttribute('alt', 'match-image');
 
-  let div = embed.slice(0);
+  const div = embed.slice(0);
   video.innerHTML = div;
 }
 
-btn.addEventListener("click", fetchData, false);
+function fetchData() {
+  fetch('https://free-football-soccer-videos.p.rapidapi.com/', options)
+    .then((response) => response.json())
+    .then((response) => showdata(response));
+}
+
+btn.addEventListener('click', fetchData);
